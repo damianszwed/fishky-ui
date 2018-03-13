ee3c925cd4a550035fc6bd80605e806f35d993a5
Damian Szwed <damian.szwed@10g.pl>
2018-03-13 18:21:03 +0100

    Two versions of flashcards list - for mobile and for desktop

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -2,17 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Flashcard = () => (
-  <div className="input-group form-row">
-    <input type="text" className="form-control col-md-4" placeholder="Question" aria-label="Question"
-           aria-describedby="basic-addon2"/>
-    <div className="input-group-prepend">
-      <span className="input-group-text">=</span>
+  <div>
+    <div className="d-none d-lg-block">
+      <div className="input-group form-row">
+        <input type="text" className="form-control col-md-4" placeholder="Question" aria-label="Question"
+               aria-describedby="basic-addon2"/>
+        <div className="input-group-prepend">
+          <span className="input-group-text">=</span>
+        </div>
+        <input type="text" className="form-control col-md-4" placeholder="Answer" aria-label="Answer"
+               aria-describedby="basic-addon2"/>
+        <div className="input-group-append">
+          <button className="btn btn-outline-secondary" type="button">Edit</button>
+          <button className="btn btn-outline-secondary" type="button">Delete</button>
+        </div>
+      </div>
     </div>
-    <input type="text" className="form-control col-md-4" placeholder="Answer" aria-label="Answer"
-           aria-describedby="basic-addon2"/>
-    <div className="input-group-append">
-      <button className="btn btn-outline-secondary" type="button">Edit</button>
-      <button className="btn btn-outline-secondary" type="button">Delete</button>
+    <div className="d-lg-none">
+      <input type="text" className="form-control col-md-4" placeholder="Question" aria-label="Question"
+             aria-describedby="basic-addon2"/>
+      <div className="input-group-prepend">
+        <span className="input-group-text">=</span>
+      </div>
+      <input type="text" className="form-control col-md-4" placeholder="Answer" aria-label="Answer"
+             aria-describedby="basic-addon2"/>
+      <div className="input-group-append">
+        <button className="btn btn-outline-secondary" type="button">Edit</button>
+        <button className="btn btn-outline-secondary" type="button">Delete</button>
+      </div>
     </div>
   </div>
 );

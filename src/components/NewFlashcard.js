54ec7d1e42f525afbb874db26bedf27065bb7a3d
Damian Szwed <damian.szwed@10g.pl>
2018-03-14 21:27:45 +0100

    basic version of adding new fishky without sending to server

--- src/components/NewFlashcard.js
+++ src/components/NewFlashcard.js
@@ -1,32 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const NewFlashcard = () => (
+const NewFlashcard = ({onSave, onChange}) => (
   <div>
     <div className="d-none d-lg-block">
       <div className="input-group form-row">
-        <input type="text" className="form-control col-md-4" placeholder="Question" aria-label="Question"
-               aria-describedby="basic-addon2"/>
+        <input type="text" className="form-control col-md-4" placeholder="Question" aria-label="question"
+               name="question" onChange={onChange} aria-describedby="basic-addon2"/>
         <div className="input-group-prepend">
           <span className="input-group-text"><i className="fas fa-arrows-alt-h fa-sm"/></span>
         </div>
-        <input type="text" className="form-control col-md-4" placeholder="Answer" aria-label="Answer"
-               aria-describedby="basic-addon2"/>
+        <input type="text" className="form-control col-md-4" placeholder="Answer" aria-label="answer"
+               name="answer" onChange={onChange} aria-describedby="basic-addon2"/>
         <div className="input-group-append">
-          <button className="btn btn-outline-success" type="button">Add flashcard</button>
+          <button className="btn btn-outline-success" type="button" onClick={onSave}>Add flashcard</button>
         </div>
       </div>
     </div>
     <div className="d-lg-none">
-      <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Question"/>
-      <input type="text" className="form-control" id="exampleFormControlInput2" placeholder="Answer"/>
+      <input type="text" className="form-control" id="question" name="question" placeholder="Question" onChange={onChange}/>
+      <input type="text" className="form-control" id="answer" name="answer" placeholder="Answer" onChange={onChange}/>
       <div className="row">
         <div className="col-md-6 offset-md-6">
-          <button className="btn btn-outline-success btn-block" type="button">Add flashcard</button>
+          <button className="btn btn-outline-success btn-block" type="button" onClick={onSave}>Add flashcard</button>
         </div>
       </div>
     </div>
   </div>
 );
+
+NewFlashcard.propTypes = {
+  onSave: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired
+};
 
 export default NewFlashcard
--- src/containers/FlashcardsContainer.js
+++ src/containers/FlashcardsContainer.js
@@ -1,16 +1,55 @@
 import React from 'react'
+import {bindActionCreators} from 'redux';
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import * as flashcardActions from '../actions/flashcardActions';
+import toastr from 'toastr';
 
 import FlashcardList from '../components/FlashcardList'
 import NewFlashcard from '../components/NewFlashcard'
 
-const FlashcardsContainer = ({flashcards}) => (
-  <div>
-    <NewFlashcard/>
-    <FlashcardList flashcards={flashcards}/>
-  </div>
-);
+class FlashcardsContainer extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      flashcard: Object.assign({}, props.flashcard),
+      errors: {},
+      saving: false
+    };
+
+    this.updateFlashcardState = this.updateFlashcardState.bind(this);
+    this.saveFlashcard = this.saveFlashcard.bind(this);
+  }
+
+  updateFlashcardState(event) {
+    const field = event.target.name;
+    let flashcard = Object.assign({}, this.state.flashcard);
+    flashcard[field] = event.target.value;
+    return this.setState({flashcard: flashcard});
+  }
+
+  saveFlashcard(event) {
+    event.preventDefault();
+
+    this.props.actions.saveFlashcard(this.state.flashcard);//TODO rename to newFlashcard
+    toastr.info("Fishky saved!");
+  }
+
+  render() {
+    const {flashcards} = this.props;
+
+    return (
+      <div>
+        <NewFlashcard
+          onSave={this.saveFlashcard}
+          onChange={this.updateFlashcardState}
+        />
+        <FlashcardList flashcards={flashcards}/>
+      </div>
+    )
+  }
+}
 
 FlashcardsContainer.propTypes = {
   flashcards: PropTypes.arrayOf(PropTypes.shape({
@@ -20,9 +59,14 @@ FlashcardsContainer.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  flashcards: state.flashcards
+  flashcards: state.flashcards,
+  actions: PropTypes.object.isRequired
 });
 
-export default connect(
-  mapStateToProps
-)(FlashcardsContainer)
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(flashcardActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FlashcardsContainer)

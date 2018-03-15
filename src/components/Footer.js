e671dec074d031e9a3dc7904de115ac17ac7f164
Damian Szwed <damian.szwed@10g.pl>
2018-03-15 14:55:24 +0100

    Footer - added about link to empty page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
-    return (
-        <div>
-            <footer className="footer">
-                <div className="container">
-                    <span className="text-muted">Author Damian Szwed</span>
-                </div>
-            </footer>
+  return (
+    <div>
+      <footer className="footer">
+        <div className="container">
+          <span className="text-muted">Author Damian Szwed</span>
+          <div className="float-right">
+            <Link to="not-implemented" className="text-muted">About</Link>
+          </div>
         </div>
-    )
+      </footer>
+    </div>
+  )
 };
 
 export default Footer;

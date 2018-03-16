3b0c5c3ee231f31c1dbf49aff2b22cfe9c3ab51e
Damian Szwed <damian.szwed@10g.pl>
2018-03-16 18:25:37 +0100

    Changed text of flashcard confirmation deletion

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -42,13 +42,13 @@ const Flashcard = ({question, answer, flashcard, onDelete}) => (
       <div className="modal-dialog" role="document">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">Delete flashcard {question}</h5>
+            <h5 className="modal-title" id="exampleModalLabel">Acknowledgement</h5>
             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div className="modal-body">
-            Are ju siur?
+            Delete flashcard {question}?
           </div>
           <div className="modal-footer">
             <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>

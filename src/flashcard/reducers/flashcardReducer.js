55e6aaace85c06585805779b33c0ab6a84f6efb4
Damian Szwed <damian.szwed@10g.pl>
2018-03-19 19:29:34 +0100

    moved action types to specifics packages

--- src/flashcard/reducers/flashcardReducer.js
+++ src/flashcard/reducers/flashcardReducer.js
@@ -1,4 +1,4 @@
-import * as types from '../../app/actions/actionTypes';
+import * as types from '../actions/actionTypes';
 
 export default function flashcardReducer(state = [], action) {
   switch (action.type) {

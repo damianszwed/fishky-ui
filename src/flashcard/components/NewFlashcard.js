5bee152f503effaa84526b9b8539cf73192bf842
Damian Szwed <damian.szwed@10g.pl>
2018-03-21 17:48:08 +0100

    handle enter on submit for each saving

--- src/flashcard/components/NewFlashcard.js
+++ src/flashcard/components/NewFlashcard.js
@@ -3,29 +3,32 @@ import PropTypes from 'prop-types'
 
 const NewFlashcard = ({onSave, onChange}) => (
   <div>
-    <div className="d-none d-lg-block">
-      <div className="input-group form-row">
-        <input type="text" className="form-control col-md-4" placeholder="Question" aria-label="question"
-               name="question" onChange={onChange} aria-describedby="basic-addon2"/>
-        <div className="input-group-prepend">
-          <span className="input-group-text"><i className="fas fa-arrows-alt-h fa-sm"/></span>
-        </div>
-        <input type="text" className="form-control col-md-4" placeholder="Answer" aria-label="answer"
-               name="answer" onChange={onChange} aria-describedby="basic-addon2"/>
-        <div className="input-group-append">
-          <button className="btn btn-outline-success" type="button" onClick={onSave}>Add flashcard</button>
-        </div>
-      </div>
-    </div>
-    <div className="d-lg-none">
-      <input type="text" className="form-control" id="question" name="question" placeholder="Question" onChange={onChange}/>
-      <input type="text" className="form-control" id="answer" name="answer" placeholder="Answer" onChange={onChange}/>
-      <div className="row">
-        <div className="col-md-6 offset-md-6">
-          <button className="btn btn-outline-success btn-block" type="button" onClick={onSave}>Add flashcard</button>
+    <form>
+      <div className="d-none d-lg-block">
+        <div className="input-group form-row">
+          <input type="text" className="form-control col-md-4" placeholder="Question" aria-label="question"
+                 name="question" onChange={onChange} aria-describedby="basic-addon2"/>
+          <div className="input-group-prepend">
+            <span className="input-group-text"><i className="fas fa-arrows-alt-h fa-sm"/></span>
+          </div>
+          <input type="text" className="form-control col-md-4" placeholder="Answer" aria-label="answer"
+                 name="answer" onChange={onChange} aria-describedby="basic-addon2"/>
+          <div className="input-group-append">
+            <button className="btn btn-outline-success" type="submit" onClick={onSave}>Add flashcard</button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="d-lg-none">
+        <input type="text" className="form-control" id="question" name="question" placeholder="Question"
+               onChange={onChange}/>
+        <input type="text" className="form-control" id="answer" name="answer" placeholder="Answer" onChange={onChange}/>
+        <div className="row">
+          <div className="col-md-6 offset-md-6">
+            <button className="btn btn-outline-success btn-block" type="submitg" onClick={onSave}>Add flashcard</button>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 );
 
--- src/learning/components/LearningQuestion.js
+++ src/learning/components/LearningQuestion.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types'
 const LearningQuestion = ({actualQuestion, onSubmit, onChange}) => {
   return (
     <div>
-      <div className="form-group">
-        <label htmlFor="answer">{actualQuestion}</label>
-        <input className="form-control" id="answer" name="answer" aria-describedby="answer"
-               placeholder="Enter answer" type="text" onChange={onChange}/>
-        <small id="answerHelp" className="form-text text-muted">Answer the question</small>
-      </div>
-      <button className="btn btn-outline-primary" onClick={onSubmit}>Submit</button>
+      <form>
+        <div className="form-group">
+          <label htmlFor="answer">{actualQuestion}</label>
+          <input className="form-control" id="answer" name="answer" aria-describedby="answer"
+                 placeholder="Enter answer" type="text" onChange={onChange}/>
+          <small id="answerHelp" className="form-text text-muted">Answer the question</small>
+        </div>
+        <button type="submit" className="btn btn-outline-primary" onClick={onSubmit}>Submit</button>
+      </form>
     </div>
   )
 };

a53f366de54acb151c5ddbc045f79811157d70d4
Damian Szwed <damian.szwed@10g.pl>
2020-05-07 19:08:18 +0200

    Changed flashcard group to set - api

--- src/sets/proxy/flashcardSetsApi.js
+++ src/sets/proxy/flashcardSetsApi.js
@@ -3,23 +3,23 @@ import url from '../../app/proxy/url';
 
 class FlashcardSetsApi {
   static getFlashcardSets() {
-    return restHelper.get(url+ '/flashcardGroups');
+    return restHelper.get(url+ '/flashcardSets');
   }
 
   static saveFlashcardSet(flashcardSet) {
-    return restHelper.post(url + '/flashcardGroups', flashcardSet);
+    return restHelper.post(url + '/flashcardSets', flashcardSet);
   }
 
   static deleteFlashcardSet(flashcardSetId) {
-    return restHelper.delete(url + '/flashcardGroups/' + flashcardSetId);
+    return restHelper.delete(url + '/flashcardSets/' + flashcardSetId);
   }
 
   static saveFlashcardInSet(flashcard, flashcardSetId) {
-    return restHelper.post(url + '/flashcardGroups/' + flashcardSetId + '/flashcards', flashcard);
+    return restHelper.post(url + '/flashcardSets/' + flashcardSetId + '/flashcards', flashcard);
   }
 
   static deleteFlashcardFromSet(flashcardId, flashcardSetId) {
-    return restHelper.delete(url + '/flashcardGroups/' + flashcardSetId + '/flashcards/'+ flashcardId);
+    return restHelper.delete(url + '/flashcardSets/' + flashcardSetId + '/flashcards/'+ flashcardId);
   }
 }
 

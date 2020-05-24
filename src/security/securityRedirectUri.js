dff43871c2735289bea4f9a19e83ca49e2fb46ca
Damian Szwed <damian.szwed@10g.pl>
2020-05-24 15:37:24 +0200

    Fixed redirect uri.

--- src/security/securityRedirectUri.js
+++ src/security/securityRedirectUri.js
@@ -2,7 +2,7 @@ export default function() {
   if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
     return 'http://localhost:3000/implicit/callback';
   } else {
-    return 'https://fishky-168.firebaseapp.com/implicit/callback'
+    return 'https://fishky.pl/implicit/callback'
   }
 }
 

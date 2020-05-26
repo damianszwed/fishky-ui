b40d9d513c521f4a78bd2349e015c5c777dbbc53
Damian Szwed <damian.szwed@10g.pl>
2020-05-26 18:03:20 +0200

    CI fix.

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -13,12 +13,12 @@ const About = () => {
             <p className="card-text">Powered by open-source software </p>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">Engineer - Damian Szwed</li>
-              <li className="list-group-item">Product Owner - Joanna Szwed</li>
+              <li className="list-group-item">Product Owner, Tester - Joanna Szwed</li>
               <li className="list-group-item">Fronted - React, Redux, Bootstrap, NPM</li>
               <li className="list-group-item">Backend - Java 11, Spring Boot (WebFlux, Security OAuth2, Data), MongoDB, Docker, Kubernetes</li>
             </ul>
             <a href="https://www.linkedin.com/in/damian-szwed/" className="card-link">Damian on LinkedIn</a>
-            <a href="#" className="card-link">Joanna on LinkedIn</a>
+            <a href="https://www.linkedin.com/in/" className="card-link">Joanna on LinkedIn</a>
           </div>
         </div>
       </div>

80520b5ee5293f26a5758a9c21d7656bd3e5e783
Damian Szwed <damian.szwed@10g.pl>
2021-09-30 16:52:24 +0200

    Here you can find the brought-in folders that you can use for the learning. - text

--- src/library/containers/LibraryContainer.js
+++ src/library/containers/LibraryContainer.js
@@ -20,7 +20,7 @@ export class LibraryContainer extends React.Component {
     return (
       <div>
         <nav className="navbar navbar-light bg-light">
-          <span className="navbar-brand mb-0 h1">Here you can find the brought in folders that you can use for (the ?) learning.</span>
+          <span className="navbar-brand mb-0 h1">Here you can find the brought-in folders that you can use for the learning.</span>
         </nav>
         {!loadingLibraryFlashcardFolders && libraryFlashcardFolders &&
           <LibraryFlashcardsFoldersComponent libraryFlashcardFolders={libraryFlashcardFolders}/>

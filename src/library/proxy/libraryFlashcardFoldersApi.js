24a976ea9b657a39e744f9d097db6af1f48654cf
Damian Szwed <damian.szwed@10g.pl>
2021-10-21 11:47:26 +0200

    LibraryFlashcardFoldersApi - minor polish

--- src/library/proxy/libraryFlashcardFoldersApi.js
+++ src/library/proxy/libraryFlashcardFoldersApi.js
@@ -2,12 +2,14 @@ import restHelper from '../../app/proxy/restHelper';
 import url from '../../app/proxy/url';
 
 class LibraryFlashcardFoldersApi {
+  static broughtInUserId = 'broughtin';
+
   static getLibraryFlashcardFolders() {
-    return restHelper.get("", url + '/owners/broughtin/flashcardFolders');
+    return restHelper.get("", url + '/owners/' + this.broughtInUserId + '/flashcardFolders');
   }
 
   static copyLibraryFolder(accessToken, flashcardFolder) {
-    return restHelper.post(accessToken, url + '/owners/broughtin/flashcardFolders/' + flashcardFolder.id + '/copy', flashcardFolder);
+    return restHelper.post(accessToken, url + '/owners/' + this.broughtInUserId + '/flashcardFolders/' + flashcardFolder.id + '/copy', flashcardFolder);
   }
 }
 

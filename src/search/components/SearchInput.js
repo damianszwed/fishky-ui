7767ef92aeb533c90de147c23c713440b4e64ce4
Damian Szwed <damian.szwed@10g.pl>
2023-04-04 15:48:09 +0200

    SearchInput.

---
+++ src/search/components/SearchInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SearchInput = (onChange) => {
+  return (
+    <div>
+      <input className="form-control mr-sm-2" placeholder="Search in my flashcards" aria-label="Search" type="text"
+             onChange={onChange}/>
+    </div>
+  )
+}
+
+SearchInput.propTypes = {
+  onChange: PropTypes.func.isRequired
+}
+
+export default SearchInput;

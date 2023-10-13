fce58c725d9c3eb7a9e1f57921812bca5a0873f1
Damian Szwed <damian.szwed@10g.pl>
2023-10-13 19:26:39 +0200

    1.0.2

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -15,7 +15,7 @@ const About = () => {
               <li className="list-group-item">Engineer - Damian Szwed</li>
               <li className="list-group-item">Product Owner, Tester - Joanna Szwed</li>
               <li className="list-group-item">Frontend - React, Redux, Bootstrap, NPM</li>
-              <li className="list-group-item">Backend - Java 11, Gradle, Spring Boot (WebFlux, Security OAuth2, Data), MongoDB, ElasticSearch, Docker, Kubernetes</li>
+              <li className="list-group-item">Backend - Java 17, Gradle, Spring Boot (WebFlux, Security OAuth2, Data), MongoDB, ElasticSearch, Docker, Kubernetes</li>
             </ul>
             <a href="https://www.linkedin.com/in/damian-szwed/" className="card-link">Damian on LinkedIn</a>
             <a href="https://www.linkedin.com/in/" className="card-link">Joanna on LinkedIn</a>
